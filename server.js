f7bd5851485b9a1d39497ac0d1b1cb86ea5058fc
AOKaizoMac <kolbywolf@gmail.com>
2022-09-12 00:21:24 -0400

    Organization

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
-const express = require('express')
+const express = require('express');
+const { url } = require('inspector');
 const app = express()
+const PORT = 3400;
 app.use(express.static('public'))
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send('/index.html'));
 
 
-const PORT = 3400;
+
+app.get('/*', (req, res) => res.send('/static/error.html'));
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
