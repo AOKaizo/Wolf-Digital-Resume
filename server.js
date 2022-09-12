f33b43c5aee725f87c097d19ea66b9334957380a
AOKaizoMac <kolbywolf@gmail.com>
2022-09-12 11:00:18 -0400

    Error page complete | Landing framework

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const fs = require('fs')
-const express = require('express');
-const { url } = require('inspector');
-const app = express()
+const fs = require("fs");
+const express = require("express");
+const { url } = require("inspector");
+const app = express();
+const path = require("path");
+// const html = require('html')
 const PORT = 3400;
-app.use(express.static('public'))
-
-app.get("/", (req, res) => res.send('/index.html'));
+app.use(express.static("public"));
 
 
-
-app.get('/*', (req, res) => res.send('/static/error.html'));
+app.get('*', (req, res) => res.sendFile('/Users/kolbywolf/Coding/JS/Junior/digitalResume/error.html'));
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-
